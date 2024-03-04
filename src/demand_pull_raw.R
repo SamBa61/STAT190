bc10ebb16a1db932c7a18fb7f39a99f81b25b91e
Sam Basala <sam.basala@drake.edu>
2024-03-04 12:23:45 -0600

    adding data

--- src/demand_pull_raw.R
+++ src/demand_pull_raw.R
@@ -20,27 +20,4 @@ demand_all_data_final <- subset(demand_all_data, select = -c(1, 3:6, 10:41))
 colnames(demand_all_data_final) <- c("Date", "Demand_MW", "Net_Generation_MW",
                                      "Total_Interchange_MW", "Region")
 
-####################################################################################################
-
-library(httr)   # For making HTTP requests
-library(jsonlite)  # For handling JSON data
-
-# Define API endpoint URL
-api_url <- "https://api.eia.gov/v2/electricity/rto/daily-region-data/data/"
-
-# Make a GET request to the API
-response <- GET(api_url)
-
-# Check if the request was successful (status code 200)
-  # Parse JSON response
-data <- content(response, "text")  # Assuming the response is JSON
-parsed_data <- fromJSON(data)
-  
-  # Check the structure of the parsed data
-str(parsed_data)
-  
-  # Convert to data frame
-df <- as.data.frame(parsed_data)
-  
-
 #write_file()
