2f63c8be1cd33b7fe1416d324216ba6b9571976c
Sam Basala <sam.basala@drake.edu>
2024-03-04 11:54:58 -0600

    split up demand files

--- src/demand_clean.R
+++ src/demand_clean.R
@@ -1,25 +1,3 @@
-# clear workspace
-rm(list = ls())
-
-# libraries
-library(dplyr)
-library(readr)
-
-# read in data
-# large .xlsx for region
-# API pull
-
-# combine separate .csv files
-demand_all_data <- list.files(path="/Users/sambasala/Desktop/STAT 190/data", full.names = TRUE) %>% 
-  lapply(read_csv) %>% 
-  bind_rows 
-
-# need to include DIBA!
-demand_all_data_final <- subset(demand_all_data, select = -c(1, 3:6, 10:41))
-
-colnames(demand_all_data_final) <- c("Date", "Demand_MW", "Net_Generation_MW",
-                                     "Total_Interchange_MW", "Region")
-
 ####################################################################################################
 
 # Data Cleaning
@@ -41,9 +19,9 @@ summary(demand_all_data_final)
 
 # Handle NAs (remove) and Aggregate by Region and Date
 aggregate_demand <- aggregate(cbind(Demand_MW, Net_Generation_MW, Total_Interchange_MW) ~ Region + Date, 
-                    data = demand_all_data_final, 
-                    FUN = sum,
-                    na.rm = TRUE)
+                              data = demand_all_data_final, 
+                              FUN = sum,
+                              na.rm = TRUE)
 
 str(aggregate_demand)
 summary(aggregate_demand)
---
+++ src/demand_pull_raw.R
@@ -0,0 +1,21 @@
+# clear workspace
+rm(list = ls())
+
+# libraries
+library(dplyr)
+library(readr)
+
+# read in data
+# large .xlsx for region
+# API pull
+
+# combine separate .csv files
+demand_all_data <- list.files(path="/Users/sambasala/Desktop/STAT 190/data", full.names = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+# need to include DIBA!
+demand_all_data_final <- subset(demand_all_data, select = -c(1, 3:6, 10:41))
+
+colnames(demand_all_data_final) <- c("Date", "Demand_MW", "Net_Generation_MW",
+                                     "Total_Interchange_MW", "Region")
