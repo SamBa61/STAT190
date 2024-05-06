e90ca9a331694b73f8b0e5caeab6cfe23609c1e9
Sam Basala <sam.basala@drake.edu>
2024-05-06 11:30:16 -0500

    deleting old stuff

--- historical/python_setup.R
+++
@@ -1,8 +0,0 @@
-library(reticulate)
-install_python(version = "3.11.0")
-virtualenv_create("my-python", python_version = "3.11.0")
-
-use_virtualenv("my-python", required = TRUE)
-virtualenv_install(envname = "my-python", "requests", ignore_installed = FALSE, pip_options= character())
-virtualenv_install(envname = "my-python", "csv", ignore_installed = FALSE, pip_options= character())
-virtualenv_install(envname = "my-python", "bs4", ignore_installed = FALSE, pip_options= character())
